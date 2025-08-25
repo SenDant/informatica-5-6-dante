6f0e81e1f2c1f1a636c4babdcf6491ff0400ab6b
SenDant <haizh310@gmail.com>
2025-08-25 19:36:19 +0000

    august 25

---
+++ checkpoint3/goblin.py
@@ -0,0 +1,16 @@
+import random
+print("WELCOME TO THE GOBLIN GAME")
+print("The best game EVER!")
+playername = input(("Insert your name: "))
+print(f"{playername}, can you find the goblin?")
+print("|_|"*5)
+goblinposition = random.randint(1, 5)
+keeptrying = True
+while keeptrying:
+    guessposition = int(input("Can you guess where the goblin is? "))
+    if guessposition == goblinposition:
+        print("Well done! You found the goblin!!!")
+        keeptrying = False
+    else:
+        print("WRONG. AGAIN!!!")
+        keeptrying
