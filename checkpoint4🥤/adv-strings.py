e2d6e3ce6e3d71a24f62e01ca07b62f7c61e30c4
SenDant <haizh310@gmail.com>
2025-09-01 19:37:38 +0000

    september 1

---
+++ checkpoint4🥤/adv-strings.py
@@ -0,0 +1,15 @@
+# #String_length
+input_string = input("Enter a string: ")
+# print(input_string)
+# print("-"*len(input_string))
+# #String_index
+# print(input_string[0])
+# print(input_string[2])
+# print(input_string[3])
+
+# i = 0
+# while i < len(input_string):
+#     print(input_string[i])
+#     i += 1
+
+print("" in input_string)
