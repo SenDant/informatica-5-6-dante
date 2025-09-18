0d840635a6398d9aa514259fd97e74e9729a1da3
SenDant <haizh310@gmail.com>
2025-09-18 19:52:22 +0000

    september 18.3

--- file_io.py
+++ file_io.py
@@ -4,7 +4,15 @@
 # for name in sorted(names):
 #     print(f"Hello, {name}.")
 
-name = input("What's your name? ")
-file = open("names.txt", "a") # w mean writing(from the beginning); a means append
-file.write(f"{name}\n") #write will work as a print for the file we're creating
-file.close() 
+# name = input("What's your name? ")
+# file = open("names.txt", "a") # w mean writing(from the beginning); a means append
+# file.write(f"{name}\n") #write will work as a print for the file we're creating
+# file.close() 
+
+# with open("names.txt", "a") as file:  # with  is a form of resuming file.close (there's no need to close it..?)
+#     file.write(f"{input("What's your name? ")}")
+
+with open("names.txt", "r") as file:     # r means reading
+    lines = file.readlines() #its turning into this: lines = ["Sen", "Song", "Hana"]
+for line in lines:
+    print(f"Hello, {line}")
