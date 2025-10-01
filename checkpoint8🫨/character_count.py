4faae2fa91ef155a868f8acfbea27f4ab4e774e7
SenDant <haizh310@gmail.com>
2025-10-01 20:20:01 +0000

    october 1

--- checkpoint8🫨/character_count.py
+++ checkpoint8🫨/character_count.py
@@ -1,13 +1,13 @@
 def main():
     print("Please, type in a message")
     message = input()
-    dictionary = {}
-    c_count(message, dictionary)
-def c_count(m, d):
-    for keys in m:
-        d.setdefault(keys, 0)
-        d[keys] += 1
+    dictionary = {} #empty dictionary
+    c_count(message, dictionary)    #function where we'll use message and the dictionary
+def c_count(m, d):  # defining said function
+    for keys in m:  # for every element in the message...
+        d.setdefault(keys, 0)   # add (to the dictionary) the characters individually, NOT repeating them, giving them a value of 0.
+        d[keys] += 1    # add +1 to each character's value.     #(if the character is repeated, is not going to be added again, but it IS going to have its value added!!!)
     print(d)
     print(len(m))
-    #print
+    
 main()
