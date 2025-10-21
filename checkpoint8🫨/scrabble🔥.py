6d1ca0f44a1872797553d65e79853820c0c40bfd
SenDant <haizh310@gmail.com>
2025-10-21 16:02:32 +0000

    october 21

--- checkpoint8🫨/scrabble🔥.py
+++ checkpoint8🫨/scrabble🔥.py
@@ -1,44 +1,55 @@
-import random
-import string
 def main():
+    import random
     alphabet = {
-        "A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1,
-        "D": 2, "G": 2,
-        "B": 3, "C": 3, "M": 3, "P": 3,
-        "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4,
-        "K": 5,
-        "J": 8, "X": 8,
-        "Q": 10, "Z": 10}
+        'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+        'D': 2, 'G': 2,
+        'B': 3, 'C': 3, 'M': 3, 'P': 3,
+        'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+        'K': 5,
+        'J': 8, 'X': 8,
+        'Q': 10, 'Z': 10
+    }
     user_letters = []
     i = 0
-    while i < 13:   
+    while i < 13:                                                       # Picks 13 random letters from the alphabet
+        user_letters.append(list(alphabet.keys())[random.randint(0,25)])
         i += 1
-        num = random.choice(string.ascii_uppercase)
-        user_letters.append(num)
     print(user_letters)
 
-    user_answer = input("Please, enter a word: ").upper()
-    n = 0
-    while n < len(user_answer):
-        if user_answer[n] in user_letters:
-            print(user_answer[n])
-            n += 1
-        else:
-            print("nope")
-            n += 1
-        
-    with open("scrabble_words.txt", "r") as file:
-        lines = file.readlines()
-        dictwords = []
-    for line in lines:
-        dictwords.append(line.replace("\n", ""))
-    if user_answer.lower() in dictwords:
-        print("Valid.")
-    else: print("That's not a word.")
+    word = input("Enter a word with these letters: ").upper()
+    score = 0
+    while True:
+        while True:                                                        
+            with open("scrabble-words.txt" ,"r") as file:           # Checks if the word is in the dictionary
+                lines = file.readlines()                            # Opens the word file of the words in the dictionary of scrabble
+            dictwords = []
+            for line in lines:
+                dictwords.append(line.replace("\n",""))
+            if word.lower() in dictwords:                           # Reads if the word is in the dictionary
+                print("Valid")
+                break
+            else: 
+                word = input("Not valid, try again: ").upper()
+                if word == "":
+                    break
 
+        if word != "":                                                  # Checks if the input is ENTER
+            
+            for letter in word:                                     # Removes the letters the user entered
+                user_letters.pop(user_letters.index(letter))        
+            
+            for value in word:                                      # Adds the value of every letter
+                score += alphabet[value]
+            
+            print(f"Your total score is: {score}")
+            print(f"Remaining letters: \n{user_letters}")
+            word = input("Enter a word with the remaining letters, press ENTER to stop: ").upper()
+            if word == "":
+                break
+        else: break
+    print(f"Thank you for playing! Your final score is {score}")
+        
     
-    o = 0
-    while o < len(user_answer):
-        print(user_letters.remove("B"))
-        o += 1
+
+
 main()
