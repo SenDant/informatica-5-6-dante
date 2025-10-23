f87e65c6094507162cc1694f695cff3b63bb4f7c
SenDant <haizh310@gmail.com>
2025-10-23 16:27:42 +0000

    october 23.3

--- checkpoint10/food_dictionary.py
+++ checkpoint10/food_dictionary.py
@@ -2,7 +2,7 @@ def main():
     word_list = [
   "banana", "milk", "soda", "cheese", "sourmilk", "juice", "sausage",
   "tomato", "cucumber", "butter", "margarine", "cheese", "sausage",
-  "soda", "sourmilk", "sourmilk", "butter", "soda", "chocolate"
+  "soda", "sourmilk", "sourmilk", "butter", "soda", "chocolate", "onion", "onion", "onion", "onion", "onion"
     ]
     count(word_list)
 def count(word_list):
