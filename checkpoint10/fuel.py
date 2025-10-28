d2cad676b41b3c62004160c11c741852caa97fed
SenDant <haizh310@gmail.com>
2025-10-28 16:39:50 +0000

    october 28

---
+++ checkpoint10/fuel.py
@@ -0,0 +1,23 @@
+def main():
+    while True:
+        try:
+            user = input("Enter a fraction: ").split("/")
+            result = (int(user[0]) / int(user[1])) * 100
+            if int(user[0]) > int(user[1]):
+                print("Invalid input.")
+                continue
+            else:
+                if result >= 99:
+                    print("F")
+                    break
+                elif result <= 1:
+                    print("E")
+                    break
+                else:
+                    print(f"{result}%")
+                    break
+        except (ZeroDivisionError, ValueError, UnboundLocalError):
+            print("Invalid input.")
+            continue
+
+main()
