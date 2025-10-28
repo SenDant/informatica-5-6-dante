bddc31f334516447d084b6a358afea5790620ccf
SenDant <haizh310@gmail.com>
2025-10-28 16:03:08 +0000

    october 28

--- checkpoint10/reading_input.py
+++ checkpoint10/reading_input.py
@@ -3,13 +3,15 @@ def main():
   print("You typed in:", number)
 
 def read_input(message, pmeter1, pmeter2):
-    try:
-        x = int(input(message))
-        if x >= pmeter1 and x <= pmeter2:
-            return x
-        else:
-            print(f"please, type a number BETWEEN {pmeter1} and {pmeter2}.")
-    except ValueError:
-        print(f"please, type a NUMBER between {pmeter1} and {pmeter2}.")
+    while True:
+        try:
+            x = int(input(message))
+            if x > pmeter1 and x < pmeter2:
+                return x
+            else:
+                print(f"please, type a number BETWEEN {pmeter1} and {pmeter2}.")
+                continue
+        except ValueError:
+            print(f"please, type a NUMBER between {pmeter1} and {pmeter2}.")
 
 main()
