739bc09e096b1e7257d6092de091a46ec467c79a
SenDant <haizh310@gmail.com>
2025-10-30 17:16:41 +0000

    october 30.2

--- checkpoint10/factorial.py
+++ checkpoint10/factorial.py
@@ -1,13 +1,16 @@
 def main():
-    x = int(input("Please, type in a positive number: "))
+    print("-------Factorial Calculator-------")
+    print("Please, type in a positive number: ")
+    x = int(input())
     factorial_calc(x)
 
 def factorial_calc(n):  # 4! = 24
     if n < 0:
         raise ValueError(f"ERROR, NEGATIVE INPUT: {n}.")
     factorial = 1
-
     for i in range(1, n + 1):
         factorial *= i
+    print("-"*34)
     print(f"{n}! = {factorial}")
+
 main()
