ee885e07b6e793a15aeb5aa6968fcff729fc1566
SenDant <haizh310@gmail.com>
2025-11-03 19:30:09 +0000

    november 3

---
+++ checkpoint11😦/libraries.py
@@ -0,0 +1,15 @@
+# import random
+# coin = random.choice(["heads", "tails"])
+# print(coin)
+# n = random.randint(1, 10)
+# print(n)
+# cards = ["Jack", "Queen", "King", "Ace"]
+# random.shuffle(cards)
+# for card in cards:
+#     print(card)
+
+# import statistics
+# print(statistics.mean([100, 90]))
+
+import sys
+print("hello,", sys.argv[1])
