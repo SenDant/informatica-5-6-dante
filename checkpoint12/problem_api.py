2d63a2835c6cfa6c045700e735a7092fbd4808cc
Sen <haizh310@gmail.com>
2025-11-17 23:11:55 +0000

    november 17

--- checkpoint12/problem_api.py
+++ checkpoint12/problem_api.py
@@ -1,6 +1,37 @@
 import requests
-API_KEY = "3HFBKUB6IZHM"
-
-user = input("Where do you wish to know the local hour?")
-timezones = requests.get(f"http://api.timezonedb.com/v2.1/convert-time-zone{API_KEY}")
-print(timezones.json())
+def main():
+    API_KEY = "12MTRNXM5HQ6"
+    while True:
+        try:
+            print("-"*55)
+            answer = input("""        Type 1 to see the timezones list,
+when you're ready, type 2 to type the desired timezone:
+""")
+            print("-"*55)
+#-/---------------------------------------------------------------------------------------------------------------------------------------/-#
+            if answer == "2":
+                timezone = input("""Type the timezone you want to see the hour. Example: 'America/Mexico_City'
+                              """)
+                info_of_timezones = requests.get(f"http://api.timezonedb.com/v2.1/get-time-zone?key={API_KEY}&format=json&by=zone&zone={timezone}").json()
+                if timezone == info_of_timezones["zoneName"]:
+                    print(f"the timezone of {timezone} is: {info_of_timezones["formatted"][11:19]}")
+                    break
+                else:
+                    print("The timezone does not exist or was written incorrectly.")
+#-/---------------------------------------------------------------------------------------------------------------------------------------/-#
+            elif answer == "1":
+                print("""                   ---CONTINENTS---
+Asia, America, Africa, Pacific, Europe
+Antarctica, Australia, Atlantic, Indian, Arctic""")
+                continent = input("Please, type the desired continent: ")
+                info_of_continents = requests.get(f"http://api.timezonedb.com/v2.1/list-time-zone?key={API_KEY}&format=json&zoneName=*{continent}*").json()
+                for all_zones in info_of_continents["zones"]:
+                    zone = all_zones["zoneName"][:len(continent)]
+                    if zone == continent:
+                        print(all_zones["zoneName"])
+            else:
+                print("Invalid input.")
+#-/---------------------------------------------------------------------------------------------------------------------------------------/-#
+        except (KeyError, TypeError, UnboundLocalError, ValueError):
+            print("An Unexpected Error Happened")
+main()
